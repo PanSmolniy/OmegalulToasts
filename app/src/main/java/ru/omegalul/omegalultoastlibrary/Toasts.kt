79fed77dd8df76123ebf26f5b26c0944dc805f39
dmitriy.smolyaninov <dmitriy.smolyaninov@doconcall.ru>
2018-08-08 17:57:38 +0300

    test

--- app/src/main/java/ru/omegalul/omegalultoastlibrary/Toasts.kt
+++ app/src/main/java/ru/omegalul/omegalultoastlibrary/Toasts.kt
@@ -10,4 +10,4 @@ fun Context?.c9Toast() =
         this.run { Toast.makeText(this, "C9", Toast.LENGTH_LONG).show() }
 
 fun Context?.c8Toast() =
-        this.run { Toast.makeText(this, "C8", Toast.LENGTH_LONG).show() }
+        this.run { Toast.makeText(this, "C8", Toast.LENGTH_LONG).show()   }
